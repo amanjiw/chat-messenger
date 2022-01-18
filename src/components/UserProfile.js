4de1ad86113738f6d1cafa250228752bec6623ea
amanjiw <ghaderi.amanj@gmail.com>
2022-01-18 23:08:16 +0330

    selected user data fetched

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./usrProfile.css";
-import userProfile from "../assets/img/profile-icon-png-898.png"
-const UserProfile = ({name , photoUrl}) => {
-  return <div className="user-profile">
+import userProfile from "../assets/img/profile-icon-png-898.png";
+
+const UserProfile = ({ name, photoUrl, email }) => {
+  const navigate = useNavigate();
+
+  const goToUserChatPage = (emailId) => {
+    if (emailId) navigate(`/${emailId}`);
+  };
+
+  return (
+    <div className="user-profile" onClick={() => goToUserChatPage(email)}>
       <div className="user-image">
-          <img src={photoUrl} alt="user profile" />
+        <img src={photoUrl} alt="user profile" />
       </div>
       <div className="usr-info">
-          <p className="user-name">{name}</p>
+        <p className="user-name">{name}</p>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default UserProfile;
